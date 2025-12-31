d67b17bef709708d28da837970fd40abf85b2771
Navyrine <wahidadinugrohoo@gmail.com>
2025-12-31 14:13:37 +0800

    menambahkan service untuk prodi

--- backend/src/model/prodiModel.js
+++ backend/src/model/prodiModel.js
@@ -14,24 +14,52 @@ async function getProdi() {
   return result;
 }
 
-async function getIdJurusan(nama_jurusan) {
-  const query = await sibema.query(
+async function addProdi(nama_jurusan, nama_prodi) {
+  const jurusanQuery = await sibema.query(
     "SELECT id_jurusan FROM jurusan WHERE LOWER (nama_jurusan) = $1",
     [nama_jurusan]
   );
-  const result = query.rows[0];
+  const id_jurusan = jurusanQuery.rows[0].id_jurusan;
 
-  return result;
-}
-
-async function addProdi(id_jurusan, nama_prodi) {
   await sibema.query(
     "INSERT INTO prodi (id_jurusan, nama_prodi) VALUES ($1, $2)",
     [id_jurusan, nama_prodi]
   );
 }
 
-async function updateProdi(id_jurusan, id_prodi, nama_prodi) {
+async function getProdiById(id_prodi) {
+  const jurusanQuery = await sibema.query(
+    "SELECT id_jurusan, nama_jurusan FROM jurusan"
+  );
+  const prodiQuery = await sibema.query(
+    `
+        SELECT 
+        prodi.id_prodi,
+        nama_jurusan,
+        nama_prodi
+        FROM prodi
+        INNER JOIN jurusan ON jurusan.id_jurusan = prodi.id_jurusan   
+        WHERE prodi.id_prodi = $1
+    `,
+    [id_prodi]
+  );
+
+  const jurusanResult = jurusanQuery.rows;
+  const prodiResult = prodiQuery.rows[0];
+
+  return {
+    jurusan: jurusanResult,
+    prodi: prodiResult,
+  };
+}
+
+async function updateProdi(nama_jurusan, id_prodi, nama_prodi) {
+  const jurusanQuery = await sibema.query(
+    "SELECT id_jurusan FROM jurusan WHERE LOWER (nama_jurusan) = $1",
+    [nama_jurusan]
+  );
+  const id_jurusan = jurusanQuery.rows[0].id_jurusan;
+
   await sibema.query(
     "UPDATE prodi SET id_jurusan = $1, nama_prodi = $2 WHERE id_prodi = $3",
     [id_jurusan, nama_prodi, id_prodi]
@@ -42,4 +70,4 @@ async function deleteProdi(id_prodi) {
   await sibema.query("DELETE FROM prodi WHERE id_prodi = $1", [id_prodi]);
 }
 
-export { getProdi, getIdJurusan, addProdi, updateProdi, deleteProdi };
+export { getProdi, addProdi, getProdiById, updateProdi, deleteProdi };
---
+++ backend/src/service/prodiService.js
@@ -0,0 +1,75 @@
+import ConflictError from "../error/ConflictError";
+import BadRequestError from "../error/BadRequestError";
+import {
+  getProdi,
+  addProdi,
+  getProdiById,
+  updateProdi,
+  deleteProdi,
+} from "../model/prodiModel";
+
+async function showProdi() {
+  const result = await getProdi();
+
+  if (result.length === 0) {
+    throw new ConflictError("Data prodi kosong");
+  }
+
+  return result;
+}
+
+async function saveProdi(nama_jurusan, nama_prodi) {
+  if (!nama_jurusan || !nama_prodi) {
+    throw new BadRequestError("Nama jurusan atau nama prodi wajib diisi");
+  }
+
+  if (typeof nama_jurusan === "string") {
+    nama_jurusan = nama_jurusan.toLowerCase().trim();
+  }
+
+  if (typeof nama_prodi === "string") {
+    nama_prodi = nama_prodi.trim();
+  }
+
+  await addProdi(nama_jurusan, nama_prodi);
+}
+
+async function showProdibyId(id_prodi) {
+  if (typeof id_prodi === "string" || isNaN(id_prodi)) {
+    id_prodi = parseInt(id_prodi);
+  }
+
+  const result = await getProdiById(id_prodi);
+
+  if (result.length === 0) {
+    throw new ConflictError("Data prodi tidak ditemukan");
+  }
+
+  return result;
+}
+
+async function editProdi(nama_jurusan, id_prodi, nama_prodi) {
+  if (typeof nama_jurusan === "string") {
+    nama_jurusan = nama_jurusan.toLowerCase().trim();
+  }
+
+  if (typeof id_prodi === "string" || isNaN(id_prodi)) {
+    id_prodi = parseInt(id_prodi);
+  }
+
+  if (typeof nama_prodi === "string") {
+    nama_prodi = nama_prodi.trim();
+  }
+
+  await updateProdi(nama_jurusan, id_prodi, nama_prodi);
+}
+
+async function removeProdi(id_prodi) {
+  if (typeof id_prodi === "string" || isNaN(id_prodi)) {
+    id_prodi = parseInt(id_prodi);
+  }
+
+  await deleteProdi(id_prodi);
+}
+
+export { showProdi, showProdibyId, saveProdi, editProdi, removeProdi };
