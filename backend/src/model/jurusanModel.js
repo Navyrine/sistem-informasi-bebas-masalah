3c0838cd18de84cb1494e8dcfa4a8fffa1be2a24
Navyrine <wahidadinugrohoo@gmail.com>
2025-12-31 10:49:53 +0800

    membuat model untuk jurusan

---
+++ backend/src/model/jurusanModel.js
@@ -0,0 +1,21 @@
+import sibema from "../config/sibema.js";
+
+async function getJurusan() {
+  const query = await sibema.query("SELECT * FROM jurusan");
+  const result = query.rows;
+
+  return result;
+}
+
+async function addJurusan(nama_jurusan) {
+  await sibema.query("INSERT INTO jurusan (nama_jurusan) VALUES ($1)", [
+    nama_jurusan,
+  ]);
+}
+
+async function updateJurusan(id_jurusan, nama_jurusan) {
+  await sibema.query(
+    "UPDATE jurusan SET nama_jurusan = $1 WHERE id_jurusan = $2",
+    [nama_jurusan, id_jurusan]
+  );
+}
