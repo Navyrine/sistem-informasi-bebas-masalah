372f34cc9c3ea991f881f840c5c92d6e690f63f9
Navyrine <wahidadinugrohoo@gmail.com>
2025-12-31 18:22:13 +0800

    menambahkan controller untuk prodi

---
+++ backend/src/controller/prodiController.js
@@ -0,0 +1,84 @@
+import BadRequestError from "../error/BadRequestError";
+import ConflictError from "../error/ConflictError";
+import {
+  showProdi,
+  showProdibyId,
+  saveProdi,
+  editProdi,
+  removeProdi,
+} from "../service/prodiService";
+
+async function presentProdi(req, res, next) {
+  try {
+    const result = showProdi();
+
+    return res.status(200).json({
+      status: 200,
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+async function presentProdibyId(req, res, next) {
+  try {
+    const id_prodi = req.params.id_prodi;
+    const result = showProdibyId(id_prodi);
+
+    return res.status(200).json({
+      status: 200,
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+async function newProdi(req, res, next) {
+  try {
+    const { nama_jurusan, nama_prodi } = req.body;
+
+    await saveProdi(nama_jurusan, nama_prodi);
+    return res.status(200).json({
+      status: 201,
+      message: "Berhasil menambahkan prodi",
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+async function changeProdi(req, res, next) {
+  try {
+    const id_prodi = req.params.id_prodi;
+    const { nama_jurusan, nama_prodi } = req.body;
+
+    await editProdi(nama_jurusan, id_prodi, nama_prodi);
+    return res.status(200).json({
+      status: 200,
+      message: "Berhasil mengubah prodi",
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+async function remove(req, res, next) {
+  try {
+    const id_prodi = req.params.id_prodi;
+
+    await removeProdi(id_prodi);
+    return res.status(201).json({
+      status: 201,
+      message: "Prodi berhasil dihapus",
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
