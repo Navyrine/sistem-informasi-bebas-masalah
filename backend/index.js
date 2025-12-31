7506b6e83152c7083fda3881317b3a7375cb1416
Navyrine <wahidadinugrohoo@gmail.com>
2025-12-31 11:36:47 +0800

    menambahkan router pada file index

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import jurusanRouter from "./src/route/jurusanRoute";
+import errorHandler from "./src/middleware/errorHandle";
 
 const app = express();
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/", jurusanRouter);
+
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log("Server running at port 3000");
 });
