dae07ba6c03a54d87c4b28e3d0bb5c6c94659448
Navyrine <wahidadinugrohoo@gmail.com>
2025-12-31 10:43:23 +0800

    setup database

---
+++ backend/src/config/sibema.js
@@ -0,0 +1,12 @@
+import pg from "pg";
+
+const sibema = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+sibema.connect();
+
+export default sibema;
