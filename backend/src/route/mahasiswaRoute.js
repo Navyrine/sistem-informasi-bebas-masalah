7831e6ed5c8a835455112b39bf1a31a00e2a66bb
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-01 21:25:01 +0800

    membuat route mahasiswa

---
+++ backend/src/route/mahasiswaRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  presentMahsiswa,
+  presentMahsiswaById,
+  newMahasiswa,
+  changeMahasiswa,
+  remove,
+} from "../controller/mahasiswaController";
+
+const mahasiswaRouter = express.Router();
+
+mahasiswaRouter.get("/mahasiswa", presentMahsiswa);
+mahasiswaRouter.get("/mahasiswa/:id_mhs", presentMahsiswaById);
+mahasiswaRouter.post("/mahasiswa", newMahasiswa);
+mahasiswaRouter.patch("/mahasiswa/:id_mhs", changeMahasiswa);
+mahasiswaRouter.delete("/mahasiswa/:id_mhs", remove);
+
+export default mahasiswaRouter;
