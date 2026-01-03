8fd90cb9ea9c74fc46b2ade7617eebcc3f9c2fbf
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-03 13:48:01 +0800

    menambahkan berita model

---
+++ backend/src/model/beritaModel.js
@@ -0,0 +1,47 @@
+import sibema from "../config/sibema.js";
+
+async function getBerita() {
+  const query = await sibema.query("SELECT * FROM berita");
+  const result = query.rows;
+
+  return result;
+}
+
+async function getBeritaById(beritaId) {
+  const query = await sibema.query(
+    "SELECT * FROM berita WHERE id_berita = $1",
+    [beritaId]
+  );
+  const result = query.rows[0];
+
+  return result;
+}
+
+async function addBerita(judul, konten, gambar) {
+  await sibema.query(
+    `
+        INSERT INTO sibema
+        (judul, kontent, gambar)
+        VALUES
+        ($1, $2, $3)    
+    `,
+    [judul, konten, gambar]
+  );
+}
+
+async function updateBerita(beritaId, judul, konten, gambar) {
+  await sibema.query(
+    `
+        UPDATE berita
+        judul = $1,
+        konten = $2,
+        gambar = $3
+        WHERE id_berita = $4
+    `,
+    [judul, konten, gambar, beritaId]
+  );
+}
+
+async function deleteBerita(beritaId) {
+  await sibema.query("DELETE FROM berita WHERE id_berita = $1", [beritaId]);
+}
