76174305d7f52b6ec6fdbb768a5f80c1c0fdde51
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-04 11:36:08 +0800

    menambahkan model tugas akhir

---
+++ backend/src/model/tugasAkhirModel.js
@@ -0,0 +1,76 @@
+import sibema from "../config/sibema";
+
+async function getTugasAkhir() {
+  const query = await sibema.query("SELECT * FROM tugas_akhir");
+  const result = query.rows;
+
+  return result;
+}
+
+async function getTugasAkhirById(taId) {
+  const query = await sibema.query("SELECT * FROM WHERE id_ta = $1", [taId]);
+  const result = query.rows[0];
+
+  return result;
+}
+
+async function addTugasAkhir(
+  lembarPersetujuan,
+  lembarPengesahan,
+  lembarKonsul1,
+  lembarKonsul2,
+  lembarRevisi,
+  rincian
+) {
+  await sibema.query(
+    `
+        INSERT INTO tugas_akhir
+        (lembar_persetujuan, lembar_pengesahan, lembar_konsul_1, lembar_konsul_2, lembar_revisi, rincian)
+        VALUES
+        ($1, $2, $3, $4, $5, $6)
+    `,
+    [
+      lembarPersetujuan,
+      lembarPengesahan,
+      lembarKonsul1,
+      lembarKonsul2,
+      lembarRevisi,
+      rincian,
+    ]
+  );
+}
+
+async function updateTugasAkhir(
+  taId,
+  lembarPersetujuan,
+  lembarPengesahan,
+  lembarKonsul1,
+  lembarKonsul2,
+  lembarRevisi,
+  rincian
+) {
+  await sibema.query(
+    `
+        UPDATE tugas_akhir
+        SET
+        lembar_persetujuan = $1,
+        lembar_pengesahan = $2,
+        lembar_konsul_1 = $3,
+        lembar_konsul_2 = $4,
+        lembar_revisi = $5,
+        rincian = $6
+        WHERE id_ta = $7    
+    `,
+    [
+      lembarPersetujuan,
+      lembarPengesahan,
+      lembarKonsul1,
+      lembarKonsul2,
+      lembarRevisi,
+      rincian,
+      taId,
+    ]
+  );
+}
+
+export { getTugasAkhir, getTugasAkhirById, addTugasAkhir, updateTugasAkhir };
