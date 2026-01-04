31e4c2362622ead58d978aa905b87c9e488d16f2
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-04 11:02:47 +0800

    menambahkan code untuk menangani gambar agar tidak tersimpan ketika proses menyimpan terdapat error dan agar tidak menghapus gambar sebelumnya ketika gambar berubah atau diupdate

--- backend/src/controller/beritaController.js
+++ backend/src/controller/beritaController.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import {
   showBerita,
   showBeritaById,
@@ -39,6 +40,14 @@ async function newBerita(req, res, next) {
       .status(201)
       .json({ status: 201, message: "Berhasil menambahkan berita" });
   } catch (err) {
+    if (req.file) {
+      fs.unlink(req.file.path, (unlinkErr) => {
+        if (unlinkErr) {
+          console.log("Gagal menghapus file: ", unlinkErr);
+        }
+      });
+    }
+
     console.log(err);
     next(err);
   }
@@ -48,13 +57,26 @@ async function changeBerita(req, res, next) {
   try {
     const beritaId = req.params.id_berita;
     const { judul, konten } = req.body;
+    const existingBerita = await showBeritaById(beritaId);
     const gambarPath = req.file ? req.file.path : null;
 
     await editBerita(beritaId, judul, konten, gambarPath);
+
+    if (req.file && existingBerita.gambar) {
+      fs.unlink(existingBerita.gambar, (error) => {
+        if (error) {
+          console.log("Gagal menghapus gambar lama: ", error);
+        }
+      });
+    }
+
     return res
       .status(200)
       .json({ status: 200, message: "Berhasil mengubah data berita" });
   } catch (err) {
+    if (req.file) {
+      fs.unlink(req.file.path, () => {});
+    }
     console.log(err);
     next(err);
   }
