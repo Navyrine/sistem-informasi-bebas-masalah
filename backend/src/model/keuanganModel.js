8392b787d83fab50d515e728b78fa21399329b87
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-06 11:30:33 +0800

    menambahkan model keuangan

---
+++ backend/src/model/keuanganModel.js
@@ -0,0 +1,37 @@
+import sibema from "../config/sibema.js";
+
+async function getKeuangan() {
+  const query = await sibema.query("SELECT * FROM keuangan");
+  const result = query.rows;
+
+  return result;
+}
+
+async function getKeuanganById(keuanganId) {
+  const query = await sibema.query(
+    "SELECT * FROM keuangan WHERE id_keuangan = $1",
+    [keuanganId]
+  );
+  const result = query.rows[0];
+
+  return result;
+}
+
+async function addKeuangan(dokumenKeuangan) {
+  await sibema.query("INSERT INTO keuangan (dokumen_keuangan) VALUES ($1)", [
+    dokumenKeuangan,
+  ]);
+}
+
+async function updateKeuangan(keuanganId, dokumenKeuangan) {
+  await sibema.query(
+    `
+        UPDATE keuangan
+        SET dokumen_keuangan = $1
+        WHERE id_keuangan = $2    
+    `,
+    [dokumenKeuangan, keuanganId]
+  );
+}
+
+export { getKeuangan, getKeuanganById, addKeuangan, updateKeuangan };
