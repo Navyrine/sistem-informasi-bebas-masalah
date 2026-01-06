e11ebfeea56de2f4603ac53e7d9dd6e74d2eeda7
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-06 11:39:12 +0800

    menambahkan keuangan service

---
+++ backend/src/service/keuanganService.js
@@ -0,0 +1,37 @@
+import ConflictError from "../error/ConflictError.js";
+import {
+  getKeuangan,
+  getKeuanganById,
+  addKeuangan,
+  updateKeuangan,
+} from "../model/keuanganModel.js";
+
+async function showKeuangan() {
+  const result = await getKeuangan();
+
+  if (result.length === 0) {
+    throw new ConflictError("Data keuangan tidak ditemukan");
+  }
+
+  return result;
+}
+
+async function showKeuanganById(keuanganId) {
+  const result = await getKeuanganById(keuanganId);
+
+  if (!result) {
+    throw new ConflictError("Data keuangan tidak ditemukan");
+  }
+
+  return result;
+}
+
+async function saveKeuangan(dokumenKeuangan) {
+  await addKeuangan(dokumenKeuangan);
+}
+
+async function editKeuangan(keuanganId, dokumenKeuangan) {
+  await updateKeuangan(dokumenKeuangan, keuanganId);
+}
+
+export { showKeuangan, showKeuanganById, saveKeuangan, editKeuangan };
