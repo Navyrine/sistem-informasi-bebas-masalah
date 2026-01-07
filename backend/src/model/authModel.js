4076332a869aa7fd421e79bfbcef5526c5280109
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-07 21:05:31 +0800

    menambahkan account model

---
+++ backend/src/model/authModel.js
@@ -0,0 +1,29 @@
+import sibema from "../config/sibema.js";
+
+async function findByUsername(username) {
+  const query = sibema.query(
+    "SELECT account.account_id, username FROM account WHERE LOWER(TRIM(username)) = $1",
+    [username]
+  );
+  const result = (await query).rows[0];
+
+  return result;
+}
+
+async function addAccount(username, email, password, role) {
+  await sibema.query(
+    `
+        INSERT INTO account
+        (username, email, password, role)
+        VALUES
+        ($1, $2, $3, $4)    
+    `,
+    [username, email, password, role]
+  );
+}
+
+async function deleteAccount(accountId) {
+  await sibema.query("DELETE FROM account WHERE id_account = $1", [accountId]);
+}
+
+export { findByUsername, addAccount, deleteAccount };
