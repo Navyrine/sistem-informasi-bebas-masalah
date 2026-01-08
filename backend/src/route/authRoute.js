5c2cb74f87ac0502207eb99702e4e2f54e713aa1
Navyrine <wahidadinugrohoo@gmail.com>
2026-01-08 13:45:21 +0800

    menambahkan auth route

---
+++ backend/src/route/authRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  registerAccount,
+  loginAccount,
+  refreshToken,
+  logoutAccount,
+} from "../controller/authController.js";
+
+const authRoute = express.Router();
+
+authRoute.post("/register", registerAccount);
+authRoute.post("/login", loginAccount);
+authRoute.post("/refresh-token", refreshToken);
+authRoute.post("/logout", logoutAccount);
+
+export default authRoute;
